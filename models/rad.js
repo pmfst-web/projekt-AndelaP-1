74b937b88b15bbe1d7542afd3b38de53316f6504
AndelaP <92169036+AndelaP@users.noreply.github.com>
2023-04-04 17:38:12 +0200

    Add files via upload

---
+++ models/rad.js
@@ -0,0 +1,12 @@
+class Rad {
+  constructor(id, student, naslov, vrsta,datumod,datumdo){
+    this.id = id
+    this.student = student;
+    this.naslov = naslov;
+    this.vrsta = vrsta;   
+    this.datumod=datumod;
+    this.datumdo=datumdo
+  }
+}
+
+export default Rad
